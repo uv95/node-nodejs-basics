550a8f9e610d4ee869c4f626b468b1c318019e2c
Uliana <ulianav95v@gmail.com>
2025-04-22 15:11:05 -0600

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fresh.txt';
+  const content = 'I am fresh and young';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, folderName, fileName);
+
+  try {
+    await access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await writeFile(filePath, content);
+    } else {
+      console.log('Error: ', error.message);
+    }
+  }
 };
 
 await create();
