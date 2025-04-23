10b3e416f076cc5357f981ff2ed7d91e6d1f86bc
Uliana <ulianav95v@gmail.com>
2025-04-22 18:18:51 -0600

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+import { cp, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolderName = 'files';
+  const destFolderName = 'files_copy';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const srcFolderDir = join(__dirname, srcFolderName);
+  const destFolderDir = join(__dirname, destFolderName);
+
+  try {
+    const isSrcFolderExists = await pathExists(srcFolderDir);
+    const isDestFolderExists = await pathExists(destFolderDir);
+
+    if (!isSrcFolderExists || isDestFolderExists) {
+      throw new Error('FS operation failed');
+    }
+
+    await cp(srcFolderDir, destFolderDir, {
+      errorOnExist: true,
+      recursive: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await copy();
+
+async function pathExists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
