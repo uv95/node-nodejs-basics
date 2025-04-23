c2d14c0ab678923f997a4043983558a76b1e0be9
Uliana <ulianav95v@gmail.com>
2025-04-23 09:01:27 -0600

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
+import { readdir, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const folderPath = join(__dirname, folderName);
+
+  try {
+    const isFolderExists = await pathExists(folderPath);
+
+    if (!isFolderExists) {
+      throw new Error('FS operation failed');
+    }
+
+    const files = await readdir(folderPath);
+    console.log(files);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await list();
+
+async function pathExists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
