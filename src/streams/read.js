30ac4f5a8325d4726d20a068f0ef5c5ff0dcbb04
Uliana <ulianav95v@gmail.com>
2025-04-24 12:50:52 -0600

    feat: add streams read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
 const read = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToRead.txt';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const filePath = join(__dirname, folderName, fileName);
+
+  try {
+    const stream = createReadStream(filePath);
+    await pipeline(stream, process.stdout);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
